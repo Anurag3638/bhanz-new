2be6dda27c763796e30e8a55d49c39e26e5dfd6e
Anurag Verma <89215725+Anurag3638@users.noreply.github.com>
2024-06-02 06:22:31 +0000

    changes

--- index.js
+++ index.js
@@ -17,12 +17,14 @@ mongoose.connect(
   },
 );
 
-
-app.engine('.hbs', exphbs.engine({
-  defaultLayout: 'main',
-  layoutsDir: join(__dirname, 'views/layouts'),
-  extname: '.hbs'
-}));
+app.engine(
+  ".hbs",
+  exphbs.engine({
+    defaultLayout: "main",
+    layoutsDir: join(__dirname, "views/layouts"),
+    extname: ".hbs",
+  }),
+);
 app.set("view engine", "hbs");
 
 app.use(express.urlencoded({ extended: false }));
@@ -37,7 +39,7 @@ app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
   const data = await product.find().limit(10).lean();
-  res.render("index", { data} );
+  res.render("index", { data });
 });
 
 app.get("/api/v/products", (req, res) => {
@@ -65,36 +67,39 @@ app.post("/api/v/products", async (req, res) => {
 app.get("/next", async (req, res) => {
   try {
     const page = Number(req.query.page) || 1;
-    const nextData = await product.find().skip((page - 1) * 10).limit(10).lean();
-    let nextPage = page+1;
-    let prevPage =  page-1;
+    const nextData = await product
+      .find()
+      .skip((page - 1) * 10)
+      .limit(10)
+      .lean();
+    let nextPage = page + 1;
+    let prevPage = page - 1;
     res.render("pages", { nextData, nextPage, prevPage });
-
   } catch (error) {
     console.error(error);
     res.status(500).send("An error occurred while fetching the data.");
   }
 });
 
-app.get("/signup", (req,res)=>{
+app.get("/signup", (req, res) => {
   res.sendFile(join(staticWebsiteFolder, "signup.html"));
 });
 
-app.post('/signup', async (req, res) => {
+app.post("/signup", async (req, res) => {
   try {
     // Create a new user document
     const newUser = new user({
-      username : req.body.username,
-      email : req.body.email,
-      phone : req.body.phone,
-      password : req.body.password,
+      username: req.body.username,
+      email: req.body.email,
+      phone: req.body.phone,
+      password: req.body.password,
     });
 
     // Save the new user document to the database
     const userData = await newUser.save();
 
     console.log(userData);
-    res.render('index'); // or any other response as needed
+    res.render("index"); // or any other response as needed
   } catch (error) {
     // Check if the error is due to duplicate key
     if (error.code === 11000 && error.keyPattern.name === 1) {
@@ -102,31 +107,30 @@ app.post('/signup', async (req, res) => {
       res.status(400).send(error);
     } else {
       console.error(error);
-      res.status(500).send('An error occurred while saving the user data.');
+      res.status(500).send("An error occurred while saving the user data.");
     }
   }
 });
-
 
 app.get("/login", (req, res) => {
   res.sendFile(join(staticWebsiteFolder, "login.html"));
 });
 
-app.post('/login', async (req, res) => {
+app.post("/login", async (req, res) => {
   try {
-    const { username , password } = req.body;
+    const { username, password } = req.body;
 
     // Check if a user with the provided email and password exists
     const checkuser = await user.findOne({ username, password });
     if (!checkuser) {
-      return res.status(401).json({ message: 'Invalid email or password' });
+      return res.status(401).json({ message: "Invalid email or password" });
     }
-
+    const data = await product.find().limit(10).lean();
     // If the email and password are correct, send a success response
-    res.render("userlogged", {username});
+    res.render("userlogged", { username, data });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Internal server error' });
+    res.status(500).json({ message: "Internal server error" });
   }
 });
 
